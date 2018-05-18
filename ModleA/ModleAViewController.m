98ac51df72b7bf2140613f6f472537cad1911f0e
hans <baoshanf@163.com>
2018-05-18 10:16:52 +0800

    组件可自行运行调试

--- ModleA/ModleAViewController.m
+++ ModleA/ModleAViewController.m
@@ -18,7 +18,7 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = NSStringFromClass([self class]);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,14 +26,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
